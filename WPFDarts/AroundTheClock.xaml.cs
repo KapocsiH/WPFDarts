a725735647083f1df9512112a02e2328b05d1d6d
Pinter Balint <pinter.balint@students.jedlik.eu>
2025-02-10 10:11:19 +0100

    2 nap munkája és semmi sem történik #3 #6

---
+++ WPFDarts/AroundTheClock.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFDarts
+{
+    /// <summary>
+    /// Interaction logic for AroundTheClock.xaml
+    /// </summary>
+    public partial class AroundTheClock : Window
+    {
+        public AroundTheClock()
+        {
+            InitializeComponent();
+        }
+    }
+}
